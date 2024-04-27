7b56ef397a1d181e65eb5059d12f121895865de4
Strcode <samcodebello@gmail.com>
2024-04-27 16:44:03 +0100

    Split delimiter and extract links method additions

--- src/main.py
+++ src/main.py
@@ -1,19 +1,10 @@
-from htmlnode import LeafNode, ParentNode
+from textnode import TextNode, split_nodes_delimiter, text_type_code, text_type_text
 
 
 def main():
-
-    node = ParentNode(
-        "p",
-        [
-            LeafNode("b", "Bold text"),
-            LeafNode(None, "Normal text"),
-            LeafNode("i", "italic text"),
-            LeafNode(None, "Normal text"),
-        ],
-    )
-    result = node.to_html()
-    print(result)
+    node = TextNode("This is text with a `code block` word", text_type_text)
+    new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+    print(new_nodes)
 
 
 main()
